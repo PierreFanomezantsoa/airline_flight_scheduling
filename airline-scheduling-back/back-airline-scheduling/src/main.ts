b542e6b841383c63f703a63ef4327344d95f13c0
PierreFanomezantsoa <rnandrasanarivopierre@gmail.com>
2026-09-24 09:45:25 +0300

    commit version 24/09/2026

--- airline-scheduling-back/back-airline-scheduling/src/main.ts
+++ airline-scheduling-back/back-airline-scheduling/src/main.ts
@@ -58,6 +58,7 @@ async function bootstrap(): Promise<void> {
 
     'http://localhost:3000',
     'http://127.0.0.1:3000',
+    'https://airline-flight.54.156.21.207.nip.io'
   ].filter(
     (
       value,
